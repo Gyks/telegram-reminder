98adab6b5a5005d08bc002881f324d53d6b6eeb9
gyks nick <gyksqop@gmail.com>
2019-09-23 17:51:07 +0300

    updated parser, ready for using in db

--- message_parser.js
+++ message_parser.js
@@ -10,44 +10,47 @@ test_msgs = [
 //TODO
 // добавить парсер сообщения для даты (21 октября)
 // юзать текущий год, обработать исключение, когда пользователь пишет дату <= текущей.
+// попробовать переписать через this и конструктор объектов.
 
-let re = /напомни\s+через\s(\d+) (час|недел|секун|месяц|дней|день).\w{0,2},?\s?(.+)/im;
+function createReminder(message, dateTypeString, amount) {
+	let now = new Date();
+	let nowString = '';
+	switch (dateTypeString) {
+		case 'час':
+			now.setHours(now.getHours() + amount);
+			nowString = now.toString();
+			break;
+		case 'дней' || 'день':
+			now.setHours(now.getHours() + amount*24);
+			nowString = now.toString();
+			break;
+		case 'недел':
+			now.setHours(now.getHours() + amount*24*7);
+			nowString = now.toString();
+			break;
+		case 'месяц':
+			now.setHours(now.getHours() + amount*24*30);
+			nowString = now.toString();
+			break;
+		case 'секун':
+			now.setSeconds(now.getSeconds() + amount);
+			nowString = now.toString();
+			break;
+		default:
+			return 'error';
+	}
+	let remindAfter = {
+		'dateObj': now,
+		'dateString': nowString,
+		'message': message,
+	};
+	return remindAfter;
+}
 
-function createReminder(inputString, amount) {
-    let now = new Date();
-    let nowString = '';
-    switch (inputString) {
-      case 'час':
-        now.setHours(now.getHours() + amount);
-        nowString = now.toString();
-        break;
-      case 'дней' || 'день':
-        now.setHours(now.getHours() + amount*24);
-        nowString = now.toString();
-        break;
-      case 'недел':
-        now.setHours(now.getHours() + amount*24*7);
-        nowString = now.toString();
-        break;
-      case 'месяц':
-        now.setHours(now.getHours() + amount*24*30);
-        nowString = now.toString();
-        break;
-      case 'секун':
-        now.setSeconds(now.getSeconds() + amount);
-        nowString = now.toString();
-        break;
-      default:
-        return 'error';
-    }
-    let remindAfter = {
-      'dateObj': now,
-      'dateString': nowString,
-    };
-    return remindAfter;
-  }
-  
-  console.log(createReminder('секун', 2999));
-  test_msgs.forEach(function(item) {
-    console.log(item.match(re));
-});
+function parseUserInput(userMessage) {
+	let re = /напомни\s+через\s(\d+) (час|недел|секун|месяц|дней|день).\w{0,2},?\s?(.+)/im;
+	let user_params = userMessage.match(re);
+	return createReminder(user_params[3], user_params[2], user_params[1]);
+}
+
+parseUserInput(test_msgs[0]);
